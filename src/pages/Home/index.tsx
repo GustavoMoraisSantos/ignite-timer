17a1b0dfc063b020e77fc53fcd1f5b88c50ec73f
Gustavo Morais Lopes dos Santos <gustavo.morais1414@gmail.com>
2024-09-09 19:44:35 -0300

    feat(home): add data list sugestions to the task input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,16 +16,26 @@ export function Home() {
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
+            list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
             type="text"
             id="task"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto" />
+            <option value="Estudos" />
+            <option value="Exercícios" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             placeholder="00"
             type="number"
             id="minutesAmount"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutos</span>
         </FormContainer>
